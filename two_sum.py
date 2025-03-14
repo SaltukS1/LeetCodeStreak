82f33e786573d3b89a18856747d94db2bad2cb9f
Your Name <saltuksanli09@gmail.com>
2025-03-14 19:57:29 +0300

    Add Two Sum solution

---
+++ two_sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_map = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            
+            num_map[num] = i
+        
+        return []
+
+# Test cases
+solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9))  # Expected: [0, 1]
+print(solution.twoSum([3, 2, 4], 6))       # Expected: [1, 2]
+print(solution.twoSum([3, 3], 6))          # Expected: [0, 1]
+
+
+

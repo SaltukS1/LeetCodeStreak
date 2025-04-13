0b1f205feb4aed377adc970f45924f6ff82f3878
Your Name <saltuksanli09@gmail.com>
2025-04-13 19:44:52 +0300

    Bug fix

---
+++ problems/24_merge_k_sorted_list/solution.py
@@ -0,0 +1,20 @@
+import heapq
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        heap = []
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))  # (değer, index, node)
+
+        dummy = ListNode(0)
+        current = dummy
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            current.next = node
+            current = current.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
